dd188d78cd5d22881c869ecf1d56e4386b0e5a1b
Niraj Karki <nirajkarki12@gmail.com>
2020-01-07 17:47:56 +0545

    icons

--- app/Viber/Listeners/ViberListener.php
+++ app/Viber/Listeners/ViberListener.php
@@ -279,7 +279,7 @@ class ViberListener
                   "ActionType" => "reply",
                   "ActionBody" => "about-the-show",
                   "BgColor" => "#f7bb3f",
-                  "Image" => "https://s18.postimg.org/9tncn0r85/sushi.png"
+                  "Image" => "https://gundruknetwork.com/finance/icons/icons-video-conference.png"
                 ),
                 array(
                   "Columns" => 2,
